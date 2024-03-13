f24868fd6bf761cd8cfbaee0fccf9e7e5721e24a
Shahid Khan <shahidseran786@gmail.com>
2024-03-14 00:56:41 +0530

    Bux fix in ThemeWatcher

--- 09TheamsWatcher/src/App.jsx
+++ 09TheamsWatcher/src/App.jsx
@@ -21,11 +21,11 @@ function App() {
   },[themeMode])
 
   return (
-    <ThemeProvider values={{ themeMode, darkTheme, lightTheme }}>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <ThemeBtn themeMode={themeMode} setThemeMode={setThemeMode} />
+            <ThemeBtn  />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
